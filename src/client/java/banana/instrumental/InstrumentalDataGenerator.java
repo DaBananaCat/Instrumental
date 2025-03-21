dc7c6fa3a73f1689c96271f5ce8d486e780702ef
Sir_BananaCat <connor@happychaps.com>
2025-03-21 15:22:20 -0500

    Initial commit

---
+++ src/client/java/banana/instrumental/InstrumentalDataGenerator.java
@@ -0,0 +1,11 @@
+package banana.instrumental;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InstrumentalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
